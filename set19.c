fa096844c2e4d979ae3481c915bbff9bd2264807
kishore1951998 <46838611+kishore1951998@users.noreply.github.com>
2019-01-21 22:17:05 +0530

    Create set19.c

---
+++ set19.c
@@ -0,0 +1,14 @@
+int main()
+{
+	int a[10],n,k,i,j=0;
+	scanf("%d",&n);
+	for(i=0;i<n;i++)
+	 scanf("%d",&a[i]);
+	scanf("%d",&k);
+	for(i=0;i<k;i++)
+	 {
+   j=j+a[i];
+   }
+	printf("%d",j);
+}
+	

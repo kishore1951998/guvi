ffe55692749a596dcb709269281d3bdc19c0c3a3
kishore1951998 <46838611+kishore1951998@users.noreply.github.com>
2019-01-21 20:50:46 +0530

    Create set13.c

---
+++ set13.c
@@ -0,0 +1,18 @@
+int main()
+{
+	char a;
+	scanf("%c",&a);
+	if((a=='a')||(a=='A'))
+	 printf("Vowel");
+	else if((a=='e')||(a=='E'))
+	 printf("Vowel");
+	else if((a=='i')||(a=='I'))
+	 printf("Vowel");
+	else if((a=='o')||(a=='O'))
+	 printf("Vowel");
+	else if((a=='U')||(a=='u'))
+	 printf("Vowel");
+	else
+	 printf("Consonent");
+	return 0;
+}

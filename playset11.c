2e2bb4c447ea888e257d6e476fad43142aa41424
kishore1951998 <46838611+kishore1951998@users.noreply.github.com>
2019-01-23 16:03:05 +0530

    Update playset11.c

--- playset11.c
+++ playset11.c
@@ -1,13 +1,22 @@
-#include<stdio.h>
+#include <stdio.h>
+
 int main()
 {
-char a[10];
-int i,n;
-scanf("%d",&n);
-scanf("%s",&a);
-for(i=n-1;i>0;i--)
-{
-printf("%c",a[10]);
+  char a[10],b[10];
+  int i,j,c=0,k=0;
+  scanf("%s",&a[i]);
+  c=0;
+  for(i=0;a[i]!='\0';i++)
+  {
+      c++;
+  }
+  k=0;
+  for(j=c-1;j>=0;j--)
+{  
+b[k]=a[j];
+ printf("%c",b[k]);
+ k++;
+ }
+ return 0;
 }
-return 0;
-}
+

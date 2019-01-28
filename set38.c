71c219394b945022518e80c19f025bee78e8786e
kishore1951998 <46838611+kishore1951998@users.noreply.github.com>
2019-01-28 22:13:38 +0530

    Create set38.c

---
+++ set38.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+int a[10],i,n;
+scanf("%d",&n);
+for(i=0;i<n;i++)
+{
+scanf("%d",&a[i]);
+}
+printf("the elements with index");
+for(i=0;i<n;i++)
+{
+printf("%d\t%d\n",i,a[i]);
+}
+}
+© 2019 GitHub, Inc.

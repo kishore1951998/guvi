46ec2b4526aa2b6610f188c330d6ab3984fc6ea0
kishore1951998 <46838611+kishore1951998@users.noreply.github.com>
2019-01-28 22:04:50 +0530

    Create set35.c

---
+++ set35.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+    int a[10],i,n,k,sum=0;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        sum=sum+a[i];
+    }
+    k=sum/n;
+    printf("%d",k);
+    }

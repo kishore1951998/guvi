9aab73c041e1015dd0445cfcd22ed47f216c3678
kishore1951998 <46838611+kishore1951998@users.noreply.github.com>
2019-02-11 07:38:42 +0530

    Create set86.c

---
+++ set86.c
@@ -0,0 +1,47 @@
+#include<stdio.h>
+
+int main(void)
+
+{
+
+int n,i,flag=0;
+
+printf("Enter n");
+
+scanf("%d",&n);
+
+for(i=2;i<=n/2;i++)
+
+{
+
+if(n%i==0)
+
+{
+
+flag=1;
+
+break;
+
+}
+
+}
+
+if(flag==0)
+
+{
+
+printf("\n no");
+
+}
+
+else
+
+{
+
+printf("\n yes");
+
+}
+
+return 0;
+
+}

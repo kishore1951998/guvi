79ece617f1ed6a712740fe89cfce16d34ab88acc
kishore1951998 <46838611+kishore1951998@users.noreply.github.com>
2019-02-11 22:56:07 +0530

    Create set106.c

---
+++ set106.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+int main()
+{
+	int n,flag=0,i;
+  clrscr();
+	scanf("%d",&n);
+	if(n%2!=0 && n>3)
+	{
+		for(i=3;i<n;i++)
+		{
+			if(n%i==0)
+			{
+				flag=1;
+				break;
+			}
+		}
+	}
+	if(flag==1)
+	{
+		printf("composite number");
+	}
+	else
+	printf("not a composite number");
+	return 0;
+}

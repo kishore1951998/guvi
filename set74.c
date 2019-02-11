04b3098fb06b6286edc3b8ea8d972dfb7c2fab72
kishore1951998 <46838611+kishore1951998@users.noreply.github.com>
2019-02-11 07:17:52 +0530

    Create set74.c

---
+++ set74.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void) {
+	int n,m,a;
+  printf("n and m value");
+	scanf("%d %d",&n,&m);
+	a=n+m;
+	if(a%2==0)
+	printf("even");
+	else
+	printf("odd");
+	return 0;
+}

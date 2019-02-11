a3d7e7547b2977f5d7ef540e5716e37550ac996b
kishore1951998 <46838611+kishore1951998@users.noreply.github.com>
2019-02-11 07:33:37 +0530

    Create set83.c

---
+++ set83.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main(void) 
+{
+	int m,n,o;
+  clrscr();
+	scanf("%d %d %d",&m,&n,&o);
+		if(m>n)
+	{
+		if(m<o)
+		printf("yes");
+	}
+	else
+	{
+		printf("no");
+	}
+	return 0;
+}

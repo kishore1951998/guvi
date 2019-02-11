054a90acbc3d83b3b8174cdc8266997f8ea0abb6
kishore1951998 <46838611+kishore1951998@users.noreply.github.com>
2019-02-11 22:18:33 +0530

    Create set95.c

---
+++ set95.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include<string.h>
+int main(void) {
+	char a[10];
+	int i,n;
+  printf("Enter a string");
+	scanf("%s",a);
+	n=strlen(a);
+	for(i=0;i<n;i++)
+	{
+		if(i%2==0)
+		{
+			printf("%c",a[i]);
+		}
+		
+	}
+	printf("\t");
+	for(i=0;i<n;i++)
+	{
+		if(i%2!=0)
+		{
+			printf("%c",a[i]);
+		}
+		
+	}
+	
+	return 0;
+}
